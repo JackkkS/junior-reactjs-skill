42195f54e6eaf9c2068a440f8dcc2899be9d73d7
JackkkS <625021000686@mail.rmutk.ac.th>
2023-09-25 11:09:35 +0700

    first commit

---
+++ src/components/FavPoke.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LinkPoke from './LinkPoke';
+
+function FavPoke(props) {
+    const { fav } = props;
+  return (
+    <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
+        {fav?.map((data, idx ) => (
+            <div key={idx}>
+            <h3>{data.name}</h3>
+            <img src={data?.sprites?.other?.home.front_default} alt={data.name} />
+            <LinkPoke />
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default FavPoke
